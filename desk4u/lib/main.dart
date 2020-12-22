9bd179db73c759e867bd592e3b9d1ca106eb743b
Sonia <sonia.jarczak@serwisprawa.pl>
2020-12-22 23:39:53 +0100

    create theme, appBar, first view

--- desk4u/lib/main.dart
+++ desk4u/lib/main.dart
@@ -4,44 +4,29 @@ void main() {
   runApp(MyApp());
 }
 
+final ThemeData _themeData = new ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Colors.grey[600],
+  accentColor: Colors.deepOrange,
+    fontFamily: 'Montserrat',
+
+);
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'desk4u',
+      theme: _themeData,
+      home: MyHomePage(title: 'desk4u'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +35,176 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _branch;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _chooseBranch() {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.grey[100],
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Image.asset(
+            'assets/images/profile.png',
+            width: 40,
+            height: 40,
+            fit: BoxFit.contain,
+            colorBlendMode: BlendMode.darken,
+            semanticLabel: 'Your image',
+          ),
+        ),
+        title: RichText(
+          text: TextSpan(
+            text: 'desk',
+            style: TextStyle(
+              color: Theme.of(context).accentColor,
+              fontWeight: FontWeight.w900,
+              fontSize: 30.0,
+            ),
+            children: [
+              TextSpan(
+                  text: '4',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30.0,
+                  ),
+              ),
+              TextSpan(
+                text: 'u',
+                style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.menu,
+              size: 30.0,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30.0),
+            child: Text(
+              'Wybierz oddział:',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w800,
+              )
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _branch = 'sudecka';
+              });
+              print('click $_branch');
+            },
+            child: Column(
+                children: [
+                Image.asset(
+                'assets/images/deviniti-logo.png',
+                width: 300,
+                height: 80,
+                fit: BoxFit.contain,
+                colorBlendMode: BlendMode.darken,
+                semanticLabel: 'Deviniti',
+                ),
+                  Text(
+                    'sudecka',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+            ],
+          ),
+          ),
+      GestureDetector(
+        onTap: () {
+          setState(() {
+            _branch = 'debowa';
+          });
+          print('click $_branch');
+        },
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          children: [
+                  Image.asset(
+                    'assets/images/deviniti-logo.png',
+                    width: 300,
+                    height: 80,
+                    fit: BoxFit.contain,
+                    colorBlendMode: BlendMode.darken,
+                    semanticLabel: 'Deviniti',
+                  ),
+                  Text(
+                    'dębowa',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      GestureDetector(
+        onTap: () {
+          setState(() {
+            _branch = 'sp-sudecka';
+          });
+          print('click $_branch');
+        },
+        child: Column(
+          children: [
+                  Image.asset(
+                    'assets/images/logo-sp.png',
+                    width: 300,
+                    height: 80,
+                    fit: BoxFit.contain,
+                    colorBlendMode: BlendMode.darken,
+                    semanticLabel: 'SerwisPrawa',
+                  ),
+                  Text(
+                    'sudecka',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+        ),
+        ],
+      ),
+    ),
+      );
   }
 }
